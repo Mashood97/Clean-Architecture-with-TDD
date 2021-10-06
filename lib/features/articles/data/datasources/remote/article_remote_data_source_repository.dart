21597bcb35de5423cf57f9b01da26f59f4998e94
invision-121 <>
2021-10-06 18:48:48 +0500

    Added Remote repository and its test cases

--- lib/features/articles/data/datasources/remote/article_remote_data_source_repository.dart
+++ lib/features/articles/data/datasources/remote/article_remote_data_source_repository.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:flutter_api_clean_architecture/core/error/exceptions.dart';
+
 import '../../models/article_model.dart';
+import 'package:dio/dio.dart';
 
 abstract class ArticleRemoteDataSourceRepository {
   /// Calls the https://jsonplaceholder.typicode.com/{todos} endpoint
@@ -6,4 +11,46 @@ abstract class ArticleRemoteDataSourceRepository {
   ///Throws a [ServerException] for all error codes
   Future<List<ArticleModel>> getAllArticles();
   Future<ArticleModel> getAllArticleById(int? articleId);
+}
+
+class ArticleRemoteDataSourceImpl implements ArticleRemoteDataSourceRepository {
+  final Dio dio;
+  ArticleRemoteDataSourceImpl({required this.dio});
+
+  @override
+  Future<ArticleModel> getAllArticleById(int? articleId) async {
+    Response response = await dio.get(
+      'https://jsonplaceholder.typicode.com/todos/$articleId',
+    );
+
+    if (response.statusCode == 200) {
+      var decode = json.decode(response.data);
+      ArticleModel article = ArticleModel.fromJson(decode);
+      return article;
+    } else {
+      throw ServerException();
+    }
+
+    // TODO: implement getAllArticleById
+  }
+
+  @override
+  Future<List<ArticleModel>> getAllArticles() async {
+    Response response = await dio.get(
+      'https://jsonplaceholder.typicode.com/todos',
+    );
+
+    if (response.statusCode == 200) {
+      final List<ArticleModel> _articles = [];
+
+      var decode = json.decode(response.data);
+      for (var article in decode) {
+        _articles.add(ArticleModel.fromJson(article));
+      }
+
+      return Future.value(_articles);
+    } else {
+      throw ServerException();
+    }
+  }
 }
---
+++ test/features/articles/data/datasources/remote/article_remote_data_source_test.dart
@@ -0,0 +1,107 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_api_clean_architecture/core/error/exceptions.dart';
+import 'package:flutter_api_clean_architecture/features/articles/data/datasources/remote/article_remote_data_source_repository.dart';
+import 'package:flutter_api_clean_architecture/features/articles/data/models/article_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../../../fixtures/fixture_reader.dart';
+
+class MockDioClient extends Mock implements Dio {}
+
+void main() {
+  late ArticleRemoteDataSourceImpl articleRemoteDataSourceImpl;
+  late MockDioClient dioClient;
+
+  setUp(() {
+    dioClient = MockDioClient();
+    articleRemoteDataSourceImpl = ArticleRemoteDataSourceImpl(dio: dioClient);
+  });
+
+  Future getSuccessApi({required String? fileName}) async {
+    when(() => dioClient.get(
+          any(),
+        )).thenAnswer((_) async => Response(
+          data: fixture(fileName),
+          statusCode: 200,
+          requestOptions: RequestOptions(path: ''),
+        ));
+  }
+
+  Future getErrorApi() async {
+    when(() => dioClient.get(
+          any(),
+        )).thenAnswer((_) async => Response(
+          data: 'something went wrong',
+          statusCode: 404,
+          requestOptions: RequestOptions(path: ''),
+        ));
+  }
+
+  group("Get All Articles", () {
+    final List<ArticleModel> tArticlesList = [];
+    List<dynamic> decoded = json.decode(fixture('articles.json'));
+    for (var article in decoded) {
+      tArticlesList.add(ArticleModel.fromJson(article));
+    }
+    test("Should get all articles when get request is performed", () async {
+      //arrange
+
+      getSuccessApi(fileName: 'articles.json');
+
+      //act
+      final result = await articleRemoteDataSourceImpl.getAllArticles();
+      //assert
+
+      verify(
+        () => dioClient.get(
+          'https://jsonplaceholder.typicode.com/todos',
+        ),
+      );
+
+      expect(result, equals(tArticlesList));
+    });
+
+    test("Should return failure Exception when api throw exception", () async {
+      getErrorApi();
+
+      final call = articleRemoteDataSourceImpl.getAllArticles;
+
+      expect(() => call(), throwsA(const TypeMatcher<ServerException>()));
+    });
+  });
+
+  group("GetArticles By ID", () {
+    final singleArticle =
+        ArticleModel.fromJson(json.decode(fixture('article.json')));
+    int tArticleId = 1;
+
+    test("Should get a single Article By Id ", () async {
+      getSuccessApi(fileName: 'article.json');
+
+      //act
+      final result =
+          await articleRemoteDataSourceImpl.getAllArticleById(tArticleId);
+      //assert
+
+      verify(
+        () => dioClient.get(
+          'https://jsonplaceholder.typicode.com/todos/$tArticleId',
+        ),
+      );
+
+      expect(result, equals(singleArticle));
+    });
+
+    test("Should return failure Exception when api throw exception", () async {
+      getErrorApi();
+
+      final call = articleRemoteDataSourceImpl.getAllArticleById;
+
+      expect(() => call(tArticleId),
+          throwsA(const TypeMatcher<ServerException>()));
+    });
+  });
+}
